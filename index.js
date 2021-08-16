e7f14d5d59c50cde90b09e44ef286061ce7d4222
Jak Major <hellojakkaiser@gmail.com>
2021-08-16 10:51:45 -0700

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return n+= 1;
+}
+
+function decrement(n) {
+    return n -= 1;
+}
+
+function makeInt(n) {
+    return parseInt (n, 10);
+    
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+    
+}
